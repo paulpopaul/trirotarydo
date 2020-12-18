8777878cfc22f3029e097bd8945ae6b84b4a01fa
paulpopaul <hallo.paul@icloud.com>
2020-12-18 16:37:20 -0300

    collect

--- trirotaryweb/settings.py
+++ trirotaryweb/settings.py
@@ -22,7 +22,7 @@ STATIC_URL = '/static/'
 MEDIA_URL = '/media/'
 
 #STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'staticfiles')]
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
